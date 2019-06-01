94a5567e6d1b1436b1f26f754a2fb2ff82ef1180
bmfmancini <sean@seanmancini.com>
2019-06-01 08:35:46 -0400

    0.1

--- twillo_sms_send.sh
+++ twillo_sms_send.sh
@@ -1,18 +1,18 @@
+#!/bin/bash
+
 ###This script will send a SMS using the twill API calling the API via CURL
 
+
+#Enter twillo account details 
 twillo_account_sid=""
-
-
-
-
-
+twillo_number=""
 
 
 ### CURL FUNCTION
 
 curl -XPOST https://api.twilio.com/2010-04-01/Accounts/$twillo_account_sid/Messages.json \
 --data-urlencode "To=+1$1" \
---data-urlencode "From=+1" \
+--data-urlencode "From=+1$twillo_number" \
 --data-urlencode "Body=$2 $3 " \
 -u '$twillo_account_sid'
 
